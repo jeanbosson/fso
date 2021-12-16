7d96747bd82b72dc180d9b417624c264ecb9782c
bossonjean <bossonjean95@gmail.com>
2021-12-16 10:15:29 +0000

    incrementation 1d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = (newValue) =>{
+    setGood(newValue)
+  }
+
+  const setToNeutral = (newNeutral) =>{
+      setNeutral(newNeutral)
+  }
+
+  const setToBad = (newBad) =>{
+    setBad(newBad)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedBack</h1>
+       
+      <button onClick={()=>setToGood(good +1)}>good</button>
+      <button onClick={()=>setToNeutral(neutral +1)}>neutral</button>
+      <button onClick={()=>setToBad(bad +1)}>bad</button>
+
+      <h2>statistic</h2>
+
+      <p> good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>
+        all = {bad + neutral + good}
+      </p>
+      <p>
+        average = {(bad + neutral + good)/3}
+      </p>
+      <p>
+        pourcentage = exo 1d
+      </p>
+
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
